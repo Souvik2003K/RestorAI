869c87d9db09fbaf2d84497d01336ed325086cbc
Souvik Moitra <souvikmoitra2003@gmail.com>
2024-07-02 21:11:46 +0530

    changed size of img-container

--- client/src/components/PrevImg.jsx
+++ client/src/components/PrevImg.jsx
@@ -3,7 +3,7 @@ import CameraAltIcon from '@mui/icons-material/CameraAlt';
 export default function PrevImg({ image }) {
   return (
     <div className="">
-      <div className="max-w-md mx-auto mt-10">
+      <div className="max-w-lg mx-auto mt-10">
         <div className="bg-white rounded-xl border-2 border-gray-400 p-6" style={{height: '450px'}}>
           {image ? <div>
             <img src={image} alt='old_img' className='storage-prop' />
