2ba06093502a267e452b5803d159adad3ce52640
Souvik Moitra <souvikmoitra2003@gmail.com>
2024-07-02 21:11:35 +0530

    changed size of img-container

--- client/src/components/NewImg.jsx
+++ client/src/components/NewImg.jsx
@@ -3,7 +3,7 @@ import ImageIcon from '@mui/icons-material/Image';
 export default function NewImg({image}) {
   return (
     <div>
-      <div className="max-w-md mx-auto mt-10">
+      <div className="max-w-lg mx-auto mt-10">
         <div className="bg-white rounded-xl border-2 border-gray-400 p-6" style={{height: '450px'}}>
         {image ? <div>
             <img src={image} alt='old_img' className='storage-prop' />
